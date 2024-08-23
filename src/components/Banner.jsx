7601f8b4e9d580cf12119548ce5267aeab1aecc6
Tri <superglue2294@gmail.com>
2024-08-22 23:26:17 -0400

    completed assignment 6  orbitReport-React

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./styling.css"
+
+const Banner = () => {
+    return (
+        <>
+            <div>
+                <h1>Orbit Report</h1>
+                <p>Click on the buttons to see the satellites in that orbit type</p>
+            </div>
+        </>
+    )
+}
+
+export default Banner
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,18 @@
+import satData from "./satData";
+import "./styling.css"
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
